917e11799690d7eaecb7fec68cf3f7da6268e9d7
Pragati Basnet <123282531+PragatiBasnet29@users.noreply.github.com>
2024-02-18 17:04:22 +0545

    Add files via upload

--- JKflipflop/JKflipflop.vhdl
+++ JKflipflop/JKflipflop.vhdl
@@ -1,58 +1,28 @@
--- library ieee;
--- use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
--- entity jk is
---     port(
---         j, k, clk, rst: in std_logic;
---         q, q_comp: out std_logic
---     );
--- end jk;
-
--- architecture jk_arch of jk is
--- begin
---     process(j, k, clk, rst) is
---         variable output: std_logic := '0';
---     begin
---         if (rst = '1') then
---             output := '0';
---         elsif (rising_edge(clk)) then
---             if (j /= k) then
---                 output := j;
---             elsif (j = '1' and k = '1') then
---                 output := not output;
---             end if;
---         end if;
---         q <= output;
---         q_comp <= not output;
---     end process;
--- end jk_arch;
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity my_jk is
-    port (
-        j, k, clk, rst: in std_logic;
-        q, q_comp: out std_logic
+ENTITY my_jk IS
+    PORT (
+        j, k, clk, rst : IN STD_LOGIC;
+        q, q_comp : OUT STD_LOGIC
     );
-end my_jk;
+END my_jk;
 
-architecture jk_arch of my_jk is
-begin
-    process(j, k, clk, rst) is
-        variable output: std_logic := '0';
-    begin
-        if (rst = '1') then
+ARCHITECTURE jk_arch OF my_jk IS
+BEGIN
+    PROCESS (j, k, clk, rst) IS
+        VARIABLE output : STD_LOGIC := '0';
+    BEGIN
+        IF (rst = '1') THEN
             output := '0';
-        elsif (rising_edge(clk)) then
-            if (j /= k) then
+        ELSIF (rising_edge(clk)) THEN
+            IF (j /= k) THEN
                 output := j;
-            elsif (j = '1' and k = '1') then
-                output := not output;
-            end if;
-        end if;
+            ELSIF (j = '1' AND k = '1') THEN
+                output := NOT output;
+            END IF;
+        END IF;
         q <= output;
-        q_comp <= not output;
-    end process;
-end jk_arch;
+        q_comp <= NOT output;
+    END PROCESS;
+END jk_arch;
